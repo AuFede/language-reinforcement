8505ece0a66b1a8686a3aee62c333deb2009706a
AuFede <afopostingbox@gmail.com>
2022-08-23 19:27:57 -0300

    lógica basada en un tiempo/setTimeout-setInterval

---
+++ timers.js
@@ -0,0 +1,11 @@
+// setInterval ejecuta un código cada cierto tiempo.
+
+// setInterval(() => {
+//     console.log('Hello World!')
+// }, 2000)
+
+// setTimeout ejecuta un código después de que pasa un tiempo.
+
+// setTimeout(() => {
+//     console.log('Hello World!')
+// }, 5000)
