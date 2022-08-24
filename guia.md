f30c6afbb0ce9c583b80932f400593dd6bc6fbee
AuFede <afopostingbox@gmail.com>
2022-08-24 20:06:17 -0300

    first module

--- guia.md
+++ guia.md
@@ -2,4 +2,4 @@ https://www.youtube.com/watch?v=i3OdKwuBjeM
 
 Primer repaso.
 
-1:09:38
+1:11:49
--- main.js
+++ main.js
@@ -14,3 +14,15 @@
 // console.log(myNumber);
 // console.log(myArray);
 // console.log(myWebAddress);
+
+// Acá importo de la carpeta math:
+
+const math = require ('./math/index')
+
+console.log(math.add(10,20))
+
+console.log(math.subtract(10,20))
+
+console.log(math.multiply(10,20))
+
+console.log(math.divide(10,20))
