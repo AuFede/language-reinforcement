e5b6309fbafea7d42dec4709547c2d819d07bbcb
AuFede <afopostingbox@gmail.com>
2022-08-25 22:48:30 -0300

    using os function

---
+++ os.js
@@ -0,0 +1,9 @@
+// Los módulos más utilizados se encuentran en la documentación de NodeJS. También hay módulos creados por la comunidad que también se pueden descargar y utlizar.
+
+const os = require('os')
+
+console.log (os.userInfo())
+console.log (os.uptime())
+console.log (os.platform())
+console.log (os.totalmem())
+console.log (os.freemem())
