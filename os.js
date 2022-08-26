d00754c821aab1fc3c49e334ba895f21d0acb8d7
AuFede <afopostingbox@gmail.com>
2022-08-25 23:13:14 -0300

    os + console.table

--- os.js
+++ os.js
@@ -1,9 +1,15 @@
 // Los módulos más utilizados se encuentran en la documentación de NodeJS. También hay módulos creados por la comunidad que también se pueden descargar y utlizar.
 
-const os = require('os')
+// const os = require('os')
 
-console.log (os.userInfo())
-console.log (os.uptime())
-console.log (os.platform())
-console.log (os.totalmem())
-console.log (os.freemem())
+// console.log (os.userInfo())
+// console.log (os.uptime())
+// console.log (os.platform())
+// console.log (os.totalmem())
+// console.log (os.freemem())
+
+// console.table({
+//     os: os.platform(),
+//     version: os.release(),
+//     totalMemory: os.totalmem()
+// })
