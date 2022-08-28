e54d11b1094daceb49776d76485d555c90457603
AuFede <afopostingbox@gmail.com>
2022-08-27 21:17:25 -0300

    fs and readFileSync()

--- fs.js
+++ fs.js
@@ -1,1 +1,8 @@
 const fs = require('fs')
+
+// readFileSync sirve para leer un archivo. El formato que ofrece es crudo, en este caso buffer. Para convertirlo por ejemplo en string, se puede usar la propiedad utf-8. También se puede usar la propiedad toString().
+
+const first = fs.readFileSync('./data/first.txt', 'utf-8')
+const second = fs.readFileSync('./data/second.txt')
+
+console.log(first, second.toString())
