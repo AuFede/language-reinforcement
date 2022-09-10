ba517268e6637aed121b710e8c4296f9cef4724d
AuFede <afopostingbox@gmail.com>
2022-09-10 19:07:34 -0300

    last commit

--- esmodule.js
+++ esmodule.js
@@ -6,3 +6,4 @@ console.log(add(10, 20));
 console.log(subtract(10, 20));
 console.log(divide(10, 20));
 console.log(multiply(10, 20));
+ 
--- fetch.js
+++ fetch.js
@@ -4,17 +4,16 @@ import fetch from "node-fetch";
 //     .then(res => res.json())
 //     .then(data => console.log(data))
 
-
 // ----- SE PUEDE USAR TOP LEVEL AWAIT -----
 
 // const loadData = async () => {
-try {
-  const res = await fetch("https://jsonplaceholder.typicode.com/posts");
-  const data = await res.json();
-  console.log(data);
-} catch (error) {
-  console.log(error);
-}
+  try {
+    const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+    const data = await res.json();
+    console.log(data);
+  } catch (error) {
+    console.log(error);
+  }
 // };
 
 // loadData();
